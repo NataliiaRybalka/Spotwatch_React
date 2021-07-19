e951d7a07c86ff2eddd64cd2fe27bb47904e6a12
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-07-19 23:36:54 +0300

    p1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [hh, setHH] = useState(0);
+  const [mm, setMM] = useState(0);
+  const [ss, setSS] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+  let watch;
+
+  if (isStarted) {
+    watch = setInterval(() => startSetTime(), 1000);
+  } else {
+    clearInterval(watch);
+  }
+
+  const onStart = () => {
+    setIsStarted(true);
+  }
+
+  const onStop = () => {
+    setIsStarted(false);
+  }
+
+  const startSetTime = () => {
+    setSS(ss + 1);
+    if (ss > 59) {
+      setMM(mm + 1 );
+      setSS(0);
+    }
+    if (mm > 59) {
+      setHH(hh + 1 );
+      setMM(0);
+    }
+    if (hh > 23) {
+      setHH(0);
+    }
+  }
+
+  const onWait = () => {
+    if (isStarted) {
+      return setIsStarted(false);
+    }
+
+    setIsStarted(true);
+  }
+
+  const onReset = () => {
+    setIsStarted(false);
+
+    setHH(0);
+    setMM(0);
+    setSS(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h1> {hh} : {mm} : {ss} </h1>
 
-export default App;
+      <div>
+        {!isStarted ? (
+          <button onClick={onStart}>Start</button>
+        ) : (
+          <button onClick={onStop}>Stop</button>
+        )}
+        <button onClick={onWait}>Wait</button>
+        <button onClick={onReset}>Reset</button>
+      </div>
+    </div>
+  )
+}
