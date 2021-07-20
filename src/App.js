7014c9a0754786e87404ccd0e301634083c25d48
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-07-20 10:28:09 +0300

    p2

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
     return () => {
       clearInterval(interval);
     };
-  }, [isActive]);
+  }, [isActive, sec]);
   
   const onStart = () => {
     setIsActive(true);
